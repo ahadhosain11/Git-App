42a32f2c2b2cf5600475912de5c98b39549af0d1
Md Ahad Hosain <mdahadhosain8@gmail.com>
2025-08-01 20:20:32 +0600

    This is my Second Commit

--- app/src/main/java/com/example/gitproject/GitDemo1.kt
+++ app/src/main/java/com/example/gitproject/GitDemo1.kt
@@ -1,2 +1,27 @@
 package com.example.gitproject
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun GitExample(modifier: Modifier = Modifier) {
+    Row (
+        modifier = modifier.fillMaxWidth().padding(20.dp),
+        horizontalArrangement = Arrangement.Center
+
+    ){
+        Text(
+            text = "Git Demo 1 "
+        )
+        Text(
+            text = "Git Demo 2 "
+        )
+
+    }
+}
